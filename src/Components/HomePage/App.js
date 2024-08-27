f5056ffef8749e83af3436bfcdb933052e5cf844
Wania Khan <waniak305@gmail.com>
2024-08-28 01:20:58 +0500

    --

--- src/Components/HomePage/App.js
+++ src/Components/HomePage/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Hero from './Hero'
 import Video from './Video'
-import StickyCards from './StickyCards'
+// import StickyCards from './StickyCards'
 import DiscoverVision from './DiscoverVision'
 import BlogTable from './BlogTable'
 import DistinctiveCulture from './DistinctiveCulture'
